e61d8d8d5f1cf32e408b548cddb25c27496aceb0
Elton Vilanculos <elltonvillas10@gmail.com>
2020-08-19 21:44:26 +0200

    Navigation Drawer

--- app/src/main/java/com/example/abantudemo/MainActivity.java
+++ app/src/main/java/com/example/abantudemo/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.abantudemo;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.abantudemo.utils.Tools;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBar actionBar;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+        initNavigationMenu();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle("Abantu");
+        Tools.setSystemBarColor(this, R.color.grey_95);
+    }
+
+    private void initNavigationMenu() {
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        // open drawer at start
+        drawer.openDrawer(GravityCompat.START);
     }
 }
