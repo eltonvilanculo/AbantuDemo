ff019c07b249cd5e9e72f096fa1eb14b082419fe
Elton Vilanculos <elltonvillas10@gmail.com>
2020-08-20 19:45:13 +0200

    Bottom nativigation layout

--- app/src/main/java/com/example/abantudemo/MainActivity.java
+++ app/src/main/java/com/example/abantudemo/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +15,6 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.abantudemo.adapter.SectionsPagerAdapter;
-import com.example.abantudemo.fragment.FragmentTabsStore;
 import com.example.abantudemo.utils.Tools;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
@@ -36,20 +34,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initToolbar();
+//        initToolbar();
         initNavigationMenu();
-        initComponent();
+
     }
 
-    private void initToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setTitle("Abantu");
-        Tools.setSystemBarColor(this, R.color.grey_95);
-    }
+//    private void initToolbar() {
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setHomeButtonEnabled(true);
+//        actionBar.setTitle("Abantu");
+//        Tools.setSystemBarColor(this, R.color.grey_95);
+//    }
 
     private void initNavigationMenu() {
         NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
@@ -64,53 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
         // open drawer at start
         drawer.openDrawer(GravityCompat.START);
-    }
-
-    private void initComponent() {
-        view_pager = (ViewPager) findViewById(R.id.view_pager);
-        tab_layout = (TabLayout) findViewById(R.id.tab_layout);
-        setupViewPager(view_pager);
-
-        tab_layout.setupWithViewPager(view_pager);
-
-        tab_layout.getTabAt(0).setIcon(R.drawable.ic_live_tv);
-        tab_layout.getTabAt(1).setIcon(R.drawable.ic_photo_camera);
-        tab_layout.getTabAt(2).setIcon(R.drawable.ic_book);
-        tab_layout.getTabAt(3).setIcon(R.drawable.ic_games);
-
-        // set icon color pre-selected
-        tab_layout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(R.color.deep_orange_500), PorterDuff.Mode.SRC_IN);
-        tab_layout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
-        tab_layout.getTabAt(2).getIcon().setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
-        tab_layout.getTabAt(3).getIcon().setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
-
-        tab_layout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                getSupportActionBar().setTitle(viewPagerAdapter.getTitle(tab.getPosition()));
-                tab.getIcon().setColorFilter(getResources().getColor(R.color.deep_orange_500), PorterDuff.Mode.SRC_IN);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                tab.getIcon().setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-    }
-
-    private void setupViewPager(ViewPager viewPager) {
-        viewPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        viewPagerAdapter.addFragment(FragmentTabsStore.newInstance(), "Novidades");    // index 0
-        viewPagerAdapter.addFragment(FragmentTabsStore.newInstance(), "Paisagens");   // index 1
-        viewPagerAdapter.addFragment(FragmentTabsStore.newInstance(), "Literatura");    // index 2
-        viewPagerAdapter.addFragment(FragmentTabsStore.newInstance(), "Jogos");    // index 3
-        viewPager.setAdapter(viewPagerAdapter);
     }
 
     @Override
