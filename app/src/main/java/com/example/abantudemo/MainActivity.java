6c0fc72a65a39b66f6848511d6572a777473e5d9
Elton Vilanculos <elltonvillas10@gmail.com>
2020-08-20 11:45:15 +0200

    Main View Pager structure

--- app/src/main/java/com/example/abantudemo/MainActivity.java
+++ app/src/main/java/com/example/abantudemo/MainActivity.java
@@ -6,17 +6,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.abantudemo.adapter.SectionsPagerAdapter;
+import com.example.abantudemo.fragment.FragmentTabsStore;
 import com.example.abantudemo.utils.Tools;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActionBar actionBar;
     private Toolbar toolbar;
+
+//    View Pager (screen slides )
+    private ViewPager view_pager;
+    private SectionsPagerAdapter viewPagerAdapter;
+    private TabLayout tab_layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         initToolbar();
         initNavigationMenu();
+        initComponent();
     }
 
     private void initToolbar() {
@@ -49,5 +64,68 @@ public class MainActivity extends AppCompatActivity {
 
         // open drawer at start
         drawer.openDrawer(GravityCompat.START);
+    }
+
+    private void initComponent() {
+        view_pager = (ViewPager) findViewById(R.id.view_pager);
+        tab_layout = (TabLayout) findViewById(R.id.tab_layout);
+        setupViewPager(view_pager);
+
+        tab_layout.setupWithViewPager(view_pager);
+
+        tab_layout.getTabAt(0).setIcon(R.drawable.ic_music);
+        tab_layout.getTabAt(1).setIcon(R.drawable.ic_movie);
+        tab_layout.getTabAt(2).setIcon(R.drawable.ic_book);
+        tab_layout.getTabAt(3).setIcon(R.drawable.ic_games);
+
+        // set icon color pre-selected
+        tab_layout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(R.color.deep_orange_500), PorterDuff.Mode.SRC_IN);
+        tab_layout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
+        tab_layout.getTabAt(2).getIcon().setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
+        tab_layout.getTabAt(3).getIcon().setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
+
+        tab_layout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                getSupportActionBar().setTitle(viewPagerAdapter.getTitle(tab.getPosition()));
+                tab.getIcon().setColorFilter(getResources().getColor(R.color.deep_orange_500), PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        viewPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(FragmentTabsStore.newInstance(), "Music");    // index 0
+        viewPagerAdapter.addFragment(FragmentTabsStore.newInstance(), "Movies");   // index 1
+        viewPagerAdapter.addFragment(FragmentTabsStore.newInstance(), "Books");    // index 2
+        viewPagerAdapter.addFragment(FragmentTabsStore.newInstance(), "Games");    // index 3
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
